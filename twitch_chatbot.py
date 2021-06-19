cc07e4685dd566e7e4beb3e99556e1b2e5a29f82
Scla <alessandro.sclafani00@gmail.com>
2021-06-20 01:05:07 +0200

    using a package

--- twitch_chatbot.py
+++ twitch_chatbot.py
@@ -1,46 +1,11 @@
-import requests
-import os
-
-import colorama
-
-from time import sleep
 from twitchio.ext import commands
-from dotenv import load_dotenv
+from utils import env, colors, get_data
 
-# colors
-colorama.init(autoreset=True)
-RED = "\033[91m"
-GREEN = "\033[92m"
-CYAN = "\033[96m"
-NOCOLOR = "\033[0m"
-LIGHT_PURPLE = "\033[95m"
 
-# loading env
-load_dotenv(os.path.join(os.path.dirname(os.path.realpath(__file__)), '.env'))
-env = {
-    "TMI_TOKEN": os.environ.get('TMI_TOKEN'),
-    "CLIENT_ID": os.environ.get('CLIENT_ID'),
-    "BOT_NICK": os.environ.get('BOT_NICK'),
-    "BOT_PREFIX": os.environ.get('BOT_PREFIX'),
-    "CHANNEL": os.environ.get('CHANNEL'),
-    "GOSUMEMORY_JSON": os.environ.get('GOSUMEMORY_JSON_URL')
-}
 # checking env
 if [True for match in env.values() if match in ['', None]]:
-    print(f"{RED}Your .env is not valid or missing.")
+    print(f"{colors.RED}Your .env is not valid or missing.")
     exit()
-
-
-def get_data() -> dict:
-    try:
-        # get data from gosumemory
-        api_data = requests.get(env['GOSUMEMORY_JSON']).json()
-        return api_data if 'error' not in api_data else None
-
-    except requests.exceptions.ConnectionError:
-        # error handling, can't connect to gosumemory
-        print(f"{RED}Could not connect to gosumemory socket!")
-        return None
 
 
 def get_map() -> dict:
@@ -61,7 +26,7 @@ def get_map() -> dict:
             "mapper": beatmap_metadata['mapper']
         }
 
-    print(f"{RED}osu is not running!")
+    print(f"{colors.RED}osu is not running!")
     return None
 
 
@@ -77,7 +42,7 @@ def get_skin() -> dict:
             "url": None
         }
 
-    print(f"{RED}osu is not running!")
+    print(f"{colors.RED}osu is not running!")
     return None
 
 
@@ -93,16 +58,16 @@ bot = commands.Bot(
 @bot.event
 async def event_ready():
     """ Runs once the bot has established a connection with Twitch """
-    print(f"{CYAN}{env['BOT_NICK']} is online!")
+    print(f"{colors.CYAN}{env['BOT_NICK']} is online!")
 
 
 @bot.event
 async def event_message(ctx):
 
     if ctx.author.name.lower() == env['BOT_NICK'].lower():
-        chat_msg = f"{LIGHT_PURPLE}{ctx.author.name}: {GREEN}{ctx.content}"
+        chat_msg = f"{colors.LIGHT_PURPLE}{ctx.author.name}: {colors.GREEN}{ctx.content}"
     else:
-        chat_msg = f"{LIGHT_PURPLE}{ctx.author.name}: {NOCOLOR}{ctx.content}"
+        chat_msg = f"{colors.LIGHT_PURPLE}{ctx.author.name}: {colors.NOCOLOR}{ctx.content}"
     print(chat_msg)
     await bot.handle_commands(ctx)
 
---
+++ utils/__init__.py
@@ -0,0 +1,37 @@
+import requests
+import os
+import colorama
+from dotenv import load_dotenv
+
+# colors
+colorama.init(autoreset=True)
+class Colors:
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    CYAN = "\033[96m"
+    NOCOLOR = "\033[0m"
+    LIGHT_PURPLE = "\033[95m"
+
+colors = Colors()
+
+# loading env
+load_dotenv(os.path.join(os.path.dirname(os.path.realpath(__file__)), '.env'))
+env = {
+    "TMI_TOKEN": os.environ.get('TMI_TOKEN'),
+    "CLIENT_ID": os.environ.get('CLIENT_ID'),
+    "BOT_NICK": os.environ.get('BOT_NICK'),
+    "BOT_PREFIX": os.environ.get('BOT_PREFIX'),
+    "CHANNEL": os.environ.get('CHANNEL'),
+    "GOSUMEMORY_JSON": os.environ.get('GOSUMEMORY_JSON_URL')
+}
+
+def get_data() -> dict:
+    try:
+        # get data from gosumemory
+        api_data = requests.get(env['GOSUMEMORY_JSON']).json()
+        return api_data if 'error' not in api_data else None
+
+    except requests.exceptions.ConnectionError:
+        # error handling, can't connect to gosumemory
+        print(f"{colors.RED}Could not connect to gosumemory socket!")
+        return None
