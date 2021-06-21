5cf4e20a7ac75356b947356047cf0f608df85518
sianogregorio <sianogregorio@isisdavinci.eu>
2021-06-21 22:43:38 +0200

    Added cache to speed up the skin command

--- skin_uploader.py
+++ skin_uploader.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from mega import Mega
 from zipfile import ZipFile
@@ -6,16 +7,15 @@ from utils import mega_env, colors, get_data
 
 
 def get_skin_url() -> str:
+    '''
+        Returns the current skin url from mega
+        If skin isn't uploaded into the mega folder, 
+        it is zipped and then uploaded to the mega folder specified in the .env
+    '''
     # checking env
     if [True for match in mega_env.values() if match in ['', None]]:
         print(f"{colors.RED}Your .env is not valid or missing.")
         exit()
-
-    # logging into mega account
-    mega = Mega()
-    mega_email = mega_env['MEGA_EMAIL']
-    mega_password = mega_env['MEGA_PASSWORD']
-    m = mega.login(mega_email, mega_password)
 
     # getting skin name and skin path from gosumemory
     api_data = get_data()
@@ -23,6 +23,23 @@ def get_skin_url() -> str:
         skin_name = api_data['settings']['folders']['skin']
         osu_path = api_data['settings']['folders']['game']
         skin_folder_path = os.path.join(osu_path, os.path.join('Skins', skin_name))
+
+    # check if the json file exists 
+    skins_dict = {}
+    if os.path.exists(os.path.join(os.path.abspath(os.getcwd()), 'skins.json')): 
+        with open("skins.json", "r") as jsonFile:
+            skins_dict = json.load(jsonFile)
+            skin_url = skins_dict.get(skin_name)
+            if skin_url:
+                return skin_url
+    else:
+        with open("skins.json","w") as jsonFile: jsonFile.write("{}")
+    
+    # logging into mega account
+    mega = Mega()
+    mega_email = mega_env['MEGA_EMAIL']
+    mega_password = mega_env['MEGA_PASSWORD']
+    m = mega.login(mega_email, mega_password)
 
     mega_file = m.find(f"{skin_name}.osk")
     mega_folder = mega_env['MEGA_OSUSKIN_FOLDER']
@@ -36,13 +53,20 @@ def get_skin_url() -> str:
 
         # uploading skin_name.osk to mega
         mega_skin = m.upload(f"{skin_name}.osk", mega_folder[0])
-        public_link = m.get_upload_link(mega_skin)
+        skin_url = m.get_upload_link(mega_skin)
+        
+        skins_dict[skin_name] = skin_url
+        with open("skins.json", "w") as jsonFile:
+            json.dump(skins_dict, jsonFile)
 
         # removing the local zip file
         os.remove(f"{skin_name}.osk")
 
-        return public_link
+        return skin_url
 
     print(f"{colors.YELLOW}The skin is already on mega!")
-    public_link = m.get_link(mega_file)
-    return public_link
+    skin_url = m.get_link(mega_file)
+    skins_dict[skin_name] = skin_url
+    with open("skins.json", "w") as jsonFile:
+        json.dump(skins_dict, jsonFile)
+    return skin_url
