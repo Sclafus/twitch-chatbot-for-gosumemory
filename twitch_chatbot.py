1b96ea40fc10f354b70723191a4870aead47e550
Scla <alessandro.sclafani00@gmail.com>
2021-06-22 01:53:28 +0200

    refactoring

--- twitch_chatbot.py
+++ twitch_chatbot.py
@@ -1,72 +1,34 @@
 from twitchio.ext import commands
-from utils import twitch_chatbot_env, colors, get_data
+from utils import data, colors
 from skin_uploader import get_skin_url
 
+twitch_data = data.get_twitch_data()
+
 # checking env
-if [True for match in twitch_chatbot_env.values() if match in ['', None]]:
-    print(f"{colors.RED}Your .env is not valid or missing.")
+if [True for match in twitch_data.values() if match in ['', None]]:
+    print(f"{colors.RED}Your .env is missing Twitch values. The program will not work without them.")
     exit()
 
 
-def get_map() -> dict:
-    '''Gets data from gosumemory, returns current beatmap informations'''
-
-    api_data = get_data()
-    if api_data:
-        # download link
-        beatmap_id = api_data['menu']['bm']['id']
-        beatmap_url = f"https://osu.ppy.sh/b/{beatmap_id}"
-
-        beatmap_metadata = api_data['menu']['bm']['metadata']
-        return {
-            "url": beatmap_url,
-            "title": beatmap_metadata['title'],
-            "artist": beatmap_metadata['artist'],
-            "diff": beatmap_metadata['difficulty'],
-            "mapper": beatmap_metadata['mapper']
-        }
-
-    print(f"{colors.RED}osu is not running!")
-    return None
-
-
-def get_skin() -> dict:
-    '''Gets data from gosumemory, returns skin informations'''
-
-    api_data = get_data()
-    if api_data:
-
-        skin_name = api_data['settings']['folders']['skin']
-        skin_url = get_skin_url()
-
-        return {
-            "skin": skin_name,
-            "url": skin_url
-        }
-
-    print(f"{colors.RED}osu is not running!")
-    return None
-
-
 bot = commands.Bot(
-    irc_token = twitch_chatbot_env['TMI_TOKEN'],
-    client_id = twitch_chatbot_env['CLIENT_ID'],
-    nick = twitch_chatbot_env['BOT_NICK'],
-    prefix = twitch_chatbot_env['BOT_PREFIX'],
-    initial_channels = [twitch_chatbot_env['CHANNEL']]
+    irc_token = twitch_data['TMI_TOKEN'],
+    client_id = twitch_data['CLIENT_ID'],
+    nick = twitch_data['BOT_NICK'],
+    prefix = twitch_data['BOT_PREFIX'],
+    initial_channels = [twitch_data['CHANNEL']]
 )
 
 
 @bot.event
 async def event_ready():
     """ Runs once the bot has established a connection with Twitch """
-    print(f"{colors.CYAN}{twitch_chatbot_env['BOT_NICK']} is online!")
+    print(f"{colors.CYAN}{twitch_data['BOT_NICK']} is online!")
 
 
 @bot.event
 async def event_message(ctx):
 
-    if ctx.author.name.lower() == twitch_chatbot_env['BOT_NICK'].lower():
+    if ctx.author.name.lower() == twitch_data['BOT_NICK'].lower():
         chat_msg = f"{colors.LIGHT_PURPLE}{ctx.author.name}: {colors.GREEN}{ctx.content}"
     else:
         chat_msg = f"{colors.LIGHT_PURPLE}{ctx.author.name}: {colors.NOCOLOR}{ctx.content}"
@@ -76,7 +38,7 @@ async def event_message(ctx):
 
 @bot.command(name='np', aliases=['nowplaying', 'song'])
 async def now_playing(ctx):
-    metadata = get_map()
+    metadata = data.get_map()
     if metadata:
         await ctx.send(f"/me {metadata['artist']} - {metadata['title']} [{metadata['diff']}] by {metadata['mapper']} {metadata['url']}")
     else:
@@ -85,13 +47,14 @@ async def now_playing(ctx):
 
 @bot.command(name='skin')
 async def skin(ctx):
-    skin = get_skin()
+    skin = data.get_skin()
     if skin:
         # removing leading and trailing whitespaces
         skin_stripped = " ".join(skin['skin'].split())
-        
-        # TODO find a way to shorten the skin url
-        await ctx.send(f"Right now {twitch_chatbot_env['CHANNEL']} is using {skin_stripped} | Link: {skin['url']}")
+        if skin['url']:
+            await ctx.send(f"Right now {twitch_data['CHANNEL']} is using {skin_stripped} | Link: {skin['url']}")
+        else:
+            await ctx.send(f"Right now {twitch_data['CHANNEL']} is using {skin_stripped} | No URL for you!")
     else:
         await ctx.send("/me Sorry bud, can't help you with that")
 
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,8 @@
 import requests
 import os
-import colorama
-from dotenv import load_dotenv
 
 # colors
+import colorama
 colorama.init(autoreset=True)
 class Colors:
     NOCOLOR = "\033[0m"
@@ -15,29 +14,6 @@ class Colors:
 
 colors = Colors()
 
-# loading env
-load_dotenv(os.path.join(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir), '.env'))
-twitch_chatbot_env = {
-    "TMI_TOKEN": os.environ.get('TMI_TOKEN'),
-    "CLIENT_ID": os.environ.get('CLIENT_ID'),
-    "BOT_NICK": os.environ.get('BOT_NICK'),
-    "BOT_PREFIX": os.environ.get('BOT_PREFIX'),
-    "CHANNEL": os.environ.get('CHANNEL'),
-    "GOSUMEMORY_JSON": os.environ.get('GOSUMEMORY_JSON_URL')
-}
-mega_env = {
-    "MEGA_EMAIL": os.environ.get('MEGA_EMAIL'),
-    "MEGA_PASSWORD": os.environ.get('MEGA_PASSWORD'),
-    "MEGA_OSUSKIN_FOLDER": os.environ.get('MEGA_OSUSKIN_FOLDER')
-}
-
-def get_data() -> dict:
-    try:
-        # get data from gosumemory
-        api_data = requests.get(twitch_chatbot_env['GOSUMEMORY_JSON']).json()
-        return api_data if 'error' not in api_data else None
-
-    except requests.exceptions.ConnectionError:
-        # error handling, can't connect to gosumemory
-        print(f"{colors.RED}Could not connect to gosumemory socket!")
-        return None
+# .env handler
+from utils.data import Data
+data = Data()
