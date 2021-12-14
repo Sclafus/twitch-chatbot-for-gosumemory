a48841dee6350814fb6e20ec6b7be2a1eadf34c2
Sclafus <alessandro.sclafani00@gmail.com>
2021-12-15 00:00:12 +0100

    this file just launches the bot now

--- twitch_chatbot.py
+++ twitch_chatbot.py
@@ -1,103 +1,10 @@
 '''
 Main program
 '''
-
-from re import findall
-
-from twitchio.ext import commands
-
-from colors import colors
-from data import data
-from utils import get_map_infos
-
-twitch_data = data.get_twitch_data()
-
-# checking env
-if [True for match in twitch_data.values() if match in ['', None]]:
-    print(f"{colors.RED}Your config file is missing Twitch values."
-          "The program will not work without them.")
-    raise KeyboardInterrupt
-
-
-bot = commands.Bot(
-    irc_token=twitch_data['TMI_TOKEN'],
-    client_id=twitch_data['CLIENT_ID'],
-    nick=twitch_data['BOT_NICK'],
-    prefix=twitch_data['BOT_PREFIX'],
-    initial_channels=[twitch_data['CHANNEL']]
-)
-
-
-@bot.event
-async def event_ready():
-    ''' Runs once the bot has established a connection with Twitch. '''
-
-    print(f"{colors.CYAN}{twitch_data['BOT_NICK']} is online!")
-
-
-@bot.event
-async def event_message(ctx):
-    ''' Runs once every time a new message appears in chat. '''
-
-    # standard chat message
-    chat_msg = f"{colors.LIGHT_PURPLE}{ctx.author.name}: {colors.NOCOLOR}{ctx.content}"
-
-    # highlighting bot messages
-    if ctx.author.name.lower() == twitch_data['BOT_NICK'].lower():
-        chat_msg = f"{colors.LIGHT_PURPLE}{ctx.author.name}: {colors.GREEN}{ctx.content}"
-
-    # map requests
-    urls = findall(
-        r'(https?://osu.ppy.sh/(b|beatmaps|beatmapsets)/[^\s]+)', ctx.content)
-    if urls:
-        for url in urls:
-            map_infos = get_map_infos(url[0])
-            chat_msg += (f"{colors.GREEN} ||| {map_infos['artist']} - "
-                         f"{map_infos['title']} [{map_infos['diff']['name']}] "
-                         f"{map_infos['diff']['stars']}★ |||{colors.NOCOLOR} ")
-
-    print(chat_msg)
-    await bot.handle_commands(ctx)
-
-
-@bot.command(name='np', aliases=['nowplaying', 'song'])
-async def now_playing(ctx):
-    ''' Sends the currently playing map and URL in chat. '''
-
-    metadata = data.get_map()
-    if metadata:
-        await ctx.send(f"/me {metadata['artist']} - {metadata['title']} "
-                       f"[{metadata['diff']}] by {metadata['mapper']} | "
-                       f"Link: {metadata['url']}")
-    else:
-        await ctx.send("/me Sorry bud, can't help you with that")
-
-
-@bot.command(name='skin')
-async def skin(ctx):
-    ''' Sends the currently used skin and URL in chat. '''
-
-    skin_dict = data.get_skin()
-    if skin_dict:
-        # removing leading and trailing whitespaces
-        skin_stripped = " ".join(skin_dict['skin'].split())
-
-        if skin_dict['url']:
-            await ctx.send(f"/me Right now {twitch_data['CHANNEL'].capitalize()} "
-                           f"is using {skin_stripped} | Link: {skin_dict['url']}")
-        else:
-            await ctx.send(f"/me Right now {twitch_data['CHANNEL'].capitalize()} "
-                           f"is using {skin_stripped} | No URL for you!")
-    else:
-        await ctx.send("/me Sorry bud, can't help you with that")
-
-
-@bot.command(name='owo')
-async def owo(ctx):
-    ''' Sends an OωO in chat. '''
-
-    await ctx.send(f"/me OωO @{ctx.author.name}")
-
+from bot.bot import Bot
+from data.gosumemory import data
 
 if __name__ == '__main__':
+    bot = Bot(access_token=data.get_twitch_data()[
+              'TMI_TOKEN'], prefix='!', channels=[data.get_twitch_data()['CHANNEL']])
     bot.run()
