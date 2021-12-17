f674d53a99628a85843bef2aa360039fc25d81ef
Sclafus <alessandro.sclafani00@gmail.com>
2021-12-18 00:28:43 +0100

    using black

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,6 @@
-'''
+"""
 Init module for colors and config handler
-'''
+"""
 
 import os
 import json
@@ -11,30 +11,32 @@ from bs4 import BeautifulSoup
 
 
 def tinyurl_shortener(full_url: str) -> str:
-    ''' Returns a shortened url for the provided url. '''
+    """Returns a shortened url for the provided url."""
 
-    api = 'https://tinyurl.com/api-create.php'
-    params = {'url': full_url}
+    api = "https://tinyurl.com/api-create.php"
+    params = {"url": full_url}
     return get(api, params).text
 
 
 def zip_skin(skin_name: str, skin_folder_path: str) -> dict:
-    ''' Zips the specified skin in a .osk file from the folder defined. '''
+    """Zips the specified skin in a .osk file from the folder defined."""
 
-    with ZipFile(f'{skin_name}.osk', 'w') as zip_file:
+    with ZipFile(f"{skin_name}.osk", "w") as zip_file:
         for root, _, files in os.walk(skin_folder_path):
             for file in files:
-                zip_file.write(os.path.join(root, file), arcname=os.path.relpath(
-                    os.path.join(root, file), skin_folder_path))
+                zip_file.write(
+                    os.path.join(root, file),
+                    arcname=os.path.relpath(os.path.join(root, file), skin_folder_path),
+                )
 
 
 def get_map_infos(url: str):
-    ''' Gets the map info using web scraping. '''
+    """Gets the map info using web scraping."""
 
     # scraping
     webpage = get(url)
-    soup = BeautifulSoup(webpage.content, 'html.parser')
-    tag = soup.find(id='json-beatmapset')
+    soup = BeautifulSoup(webpage.content, "html.parser")
+    tag = soup.find(id="json-beatmapset")
 
     if not tag:
         return None
@@ -63,16 +65,19 @@ def get_map_infos(url: str):
     #     } for diff_data in json_data['beatmaps']]
 
     # finding top diff
-    diff = max([{'name': diff_data['version'],
-                 'stars': diff_data['difficulty_rating'],
-                 'id': diff_data['id'],
-                 'bpm': diff_data['bpm'],
-                 'ar': diff_data['ar'],
-                 'cs': diff_data['cs']
-                 } for diff_data in json_data['beatmaps']], key=lambda x: x['stars'])
+    diff = max(
+        [
+            {
+                "name": diff_data["version"],
+                "stars": diff_data["difficulty_rating"],
+                "id": diff_data["id"],
+                "bpm": diff_data["bpm"],
+                "ar": diff_data["ar"],
+                "cs": diff_data["cs"],
+            }
+            for diff_data in json_data["beatmaps"]
+        ],
+        key=lambda x: x["stars"],
+    )
 
-    return {
-        'artist': json_data['artist'],
-        'title': json_data['title'],
-        'diff': diff
-    }
+    return {"artist": json_data["artist"], "title": json_data["title"], "diff": diff}
