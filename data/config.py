1a5aeb09d7ad5a5dc0ab6a430ffcf99a1aa8d729
Sclafus <alessandro.sclafani00@gmail.com>
2021-12-20 12:24:56 +0100

    handling missing or incomplete config file

--- data/config.py
+++ data/config.py
@@ -1,39 +1,49 @@
-from dotenv import load_dotenv
 from os import environ
+from dotenv import load_dotenv
 
 
 class Config:
-
-    def __init__(self, filename: str = 'config.txt'):
+    def __init__(self, filename: str = "config.txt"):
         load_dotenv(filename)
 
-        self.TMI_TOKEN = environ.get('TMI_TOKEN')
-        self.CLIENT_ID = environ.get('CLIENT_ID')
-        self.BOT_NICK = environ.get('BOT_NICK')
-        self.BOT_PREFIX = environ.get('BOT_PREFIX')
-        self.CHANNEL = environ.get('CHANNEL')
-        self.GOSUMEMORY_JSON = environ.get('GOSUMEMORY_JSON_URL')
-        self.MEGA_EMAIL = environ.get('MEGA_EMAIL')
-        self.MEGA_PASSWORD = environ.get('MEGA_PASSWORD')
-        self.MEGA_FOLDER = environ.get('MEGA_FOLDER')
+        self.TMI_TOKEN = environ.get("TMI_TOKEN")
+        self.CLIENT_ID = environ.get("CLIENT_ID")
+        self.BOT_NICK = environ.get("BOT_NICK")
+        self.BOT_PREFIX = environ.get("BOT_PREFIX")
+        self.CHANNEL = environ.get("CHANNEL")
+        self.GOSUMEMORY_JSON = environ.get("GOSUMEMORY_JSON_URL")
+        self.MEGA_EMAIL = environ.get("MEGA_EMAIL")
+        self.MEGA_PASSWORD = environ.get("MEGA_PASSWORD")
+        self.MEGA_FOLDER = environ.get("MEGA_FOLDER")
 
     def get_mega_data(self) -> dict:
-        ''' Returns mega config data. '''
+        """Returns mega config data."""
 
         return {
-            'MEGA_EMAIL': self.MEGA_EMAIL,
-            'MEGA_PASSWORD': self.MEGA_PASSWORD,
-            'MEGA_FOLDER': self.MEGA_FOLDER
+            "MEGA_EMAIL": self.MEGA_EMAIL,
+            "MEGA_PASSWORD": self.MEGA_PASSWORD,
+            "MEGA_FOLDER": self.MEGA_FOLDER,
         }
 
     def get_twitch_data(self) -> dict:
-        ''' Returns twitch config data. '''
+        """Returns twitch config data."""
 
         return {
-            'TMI_TOKEN': self.TMI_TOKEN,
-            'CLIENT_ID': self.CLIENT_ID,
-            'BOT_NICK': self.BOT_NICK,
-            'BOT_PREFIX': self.BOT_PREFIX,
-            'CHANNEL': self.CHANNEL,
+            "TMI_TOKEN": self.TMI_TOKEN,
+            "CLIENT_ID": self.CLIENT_ID,
+            "BOT_NICK": self.BOT_NICK,
+            "BOT_PREFIX": self.BOT_PREFIX,
+            "CHANNEL": self.CHANNEL,
         }
 
+    def is_config_usable(self) -> bool:
+        """Checks if values are valid"""
+        return self.is_twitch_config_valid() and self.GOSUMEMORY_JSON is not None
+
+    def is_twitch_config_valid(self) -> bool:
+        """Checks if the twitch values are valid"""
+        return all(value is not None for value in self.get_twitch_data().values())
+
+    def is_mega_config_valid(self) -> bool:
+        """Checks if the mega values are valid"""
+        return all(value is not None for value in self.get_mega_data().values())
--- twitch_chatbot.py
+++ twitch_chatbot.py
@@ -1,11 +1,28 @@
-'''
+"""
 Main program
-'''
+"""
+import sys
+
 from bot.bot import Bot
 from data.config import Config
+from outputs.outputs import Outputs
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     config = Config()
-    bot = Bot(access_token=config.get_twitch_data()[
-              'TMI_TOKEN'], prefix='!', channels=[config.get_twitch_data()['CHANNEL']])
+
+    if not config.is_config_usable():
+        Outputs().print_error("The config file is missing.")
+        sys.exit(1)
+
+    if not config.is_mega_config_valid():
+        Outputs().print_info(
+            "Mega config is missing or invalid."
+        )
+
+    twitch_data = config.get_twitch_data()
+    bot = Bot(
+        access_token=twitch_data["TMI_TOKEN"],
+        prefix=twitch_data["BOT_PREFIX"],
+        channels=[twitch_data["CHANNEL"]],
+    )
     bot.run()
